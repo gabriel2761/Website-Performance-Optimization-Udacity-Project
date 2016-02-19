28fe823877e5c8e0e3bbd7741cdce99a49993091
Gabriel Mojica <niko.mojica@live.com>
2016-02-20 03:25:53 +1100

    fix: Move getElementById for random div outside loop

--- views/js/main.js
+++ views/js/main.js
@@ -436,8 +436,8 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
+var pizzasDiv = document.getElementById("randomPizzas");
 for (var i = 2; i < 20; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
