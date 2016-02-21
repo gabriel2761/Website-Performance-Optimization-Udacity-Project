0bc2eda12732f81b23cb94076772c6b50f9eecc6
Gabriel Mojica <niko.mojica@live.com>
2016-02-22 01:46:37 +1100

    chore: Add gulp-imagemin and imagemin-pngquant

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 var gulp = require('gulp'),
 	htmlmin = require('gulp-htmlmin'),
-	inline = require('gulp-inline-source');
+	inline = require('gulp-inline-source'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant');
+
+gulp.task('imagemin', function() {
+	return gulp.src('app/img/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('dist/img'))
+});
 
 gulp.task('minify', function() {
 	return gulp.src('app/**/*.html')
