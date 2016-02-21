c6a8b8dd81e5824773c0ca9e7ee1446df6233a06
Gabriel Mojica <niko.mojica@live.com>
2016-02-22 01:26:41 +1100

    chore: Add gulp-htmlmin and gulp-inline-source

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	htmlmin = require('gulp-htmlmin'),
+	inline = require('gulp-inline-source');
+
+gulp.task('minify', function() {
+	return gulp.src('app/**/*.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(inline())
+    	.pipe(gulp.dest('dist'));
+});
 
 gulp.task('default', function() {
 	console.log('hello');
